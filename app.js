dd67c70ba21a24cd4b3769c2e8f732b4fbcfd05a
markhamilton90 <msh387@gmail.com>
2015-11-06 15:18:03 -0600

    full angular functionality

---
+++ app.js
@@ -0,0 +1,42 @@
+
+angular.module('waitstaff', [])
+	.controller('waitstaffController', function($scope) {
+
+		$scope.price;
+		$scope.taxRate;
+		$scope.tip;
+
+		$scope.subTotal = 0.00;
+		$scope.displayTip = 0;
+		$scope.total = 0.00;
+
+		$scope.tipTotal = 0.00;
+		$scope.mealCount = 0;
+		$scope.averageTip = 0.00;
+
+		$scope.submit = function() {
+			$scope.subTotal = $scope.price + ($scope.taxRate/100 * $scope.price);
+			$scope.displayTip = $scope.tip/100 * $scope.subTotal;
+			$scope.total = $scope.subTotal + $scope.displayTip;
+
+			$scope.tipTotal += ($scope.tip/100 * $scope.subTotal);
+			$scope.mealCount++;
+			$scope.averageTip = $scope.tipTotal/$scope.mealCount;
+		}
+
+		$scope.cancel = function() {
+			$scope.price = ""; 
+			$scope.taxRate = ""; 
+			$scope.tip = "";
+		}
+
+		$scope.reset = function() {
+			$scope.cancel();
+			$scope.subTotal = 0.00;
+			$scope.displayTip = 0;
+			$scope.total = 0.00;
+			$scope.tipTotal = 0.00;
+			$scope.mealCount = 0;
+			$scope.averageTip = 0.00;
+		}
+	})
