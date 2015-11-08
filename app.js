181100a0ff7da97cecf14443be4a7275f57316d6
markhamilton90 <msh387@gmail.com>
2015-11-07 21:18:19 -0600

    fixed error message behavior

--- app.js
+++ app.js
@@ -6,33 +6,38 @@ angular.module('waitstaff', [])
 		$scope.taxRate;
 		$scope.tip;
 
-		$scope.subTotal = 0.00;
+		$scope.subTotal = 0;
 		$scope.displayTip = 0;
-		$scope.total = 0.00;
+		$scope.total = 0;
 
-		$scope.tipTotal = 0.00;
+		$scope.tipTotal = 0;
 		$scope.mealCount = 0;
-		$scope.averageTip = 0.00;
+		$scope.averageTip = 0;
 
 		$scope.submit = function() {
 			$scope.subTotal = $scope.price + ($scope.taxRate/100 * $scope.price);
-			$scope.displayTip = $scope.tip/100 * $scope.subTotal;
+			$scope.displayTip = ($scope.tip/100) * $scope.subTotal;
 			$scope.total = $scope.subTotal + $scope.displayTip;
 
-			$scope.tipTotal += ($scope.tip/100 * $scope.subTotal);
+			$scope.tipTotal += $scope.tip/100 * $scope.subTotal;
 			$scope.mealCount++;
 			$scope.averageTip = $scope.tipTotal/$scope.mealCount;
 		}
 
-		$scope.reset = function() {
+		$scope.cancel = function() {
+			$scope.waitForm.$submitted = false;
 			$scope.price = ""; 
 			$scope.taxRate = ""; 
 			$scope.tip = "";
-			$scope.subTotal = 0.00;
+		}
+
+		$scope.reset = function() {
+			$scope.cancel();
+			$scope.subTotal = 0;
 			$scope.displayTip = 0;
-			$scope.total = 0.00;
-			$scope.tipTotal = 0.00;
+			$scope.total = 0;
+			$scope.tipTotal = 0;
 			$scope.mealCount = 0;
-			$scope.averageTip = 0.00;
+			$scope.averageTip = 0;
 		}
 	})
